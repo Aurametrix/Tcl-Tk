dac1c80ed59b8c0044d38abc061926c3d39b9bbc
Aurametrix <irene@aurametrix.com>
2016-01-13 20:59:54 -0800

    iterative solution

--- PUZZLES/step.tcl
+++ PUZZLES/step.tcl
@@ -1,13 +1,5 @@
-set level 41
-set prob 0.5001
-proc step {} {
-    global level prob steps
-    incr steps
-    if {rand() < $prob} {
-	incr level 1
-	return 1
-    } else {
-	incr level -1
-	return 0
+proc step-up-iter {} {
+    for {incr d} {$d} {incr d} {
+	incr d [set s -[step]]; incr d $s
     }
 }
